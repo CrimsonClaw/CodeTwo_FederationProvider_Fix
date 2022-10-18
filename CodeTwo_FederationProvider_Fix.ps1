ad9ccc7edb388266c2744bcd6c35053e225e78ca
CrimsonClaw <116062339+CrimsonClaw@users.noreply.github.com>
2022-10-18 09:23:43 +0200

    Add files via upload

---
+++ CodeTwo_FederationProvider_Fix.ps1
@@ -0,0 +1,10 @@
+﻿$ts = New-TimeSpan -Days 0 -Hours 0 -Minutes 1
+$setTime = (Get-Date) + $ts
+$fTime = $setTime.ToString('HH:mm')
+
+$Trigger= New-ScheduledTaskTrigger -At $fTime -Once
+$User= [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+
+$Action= Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Office\16.0\Common\Identity\Identities\*_ADAL" -Name "FederationProvider" -Value ""
+
+Register-ScheduledTask -TaskName "CodeTwo_FederationProvider_Fix" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest -Force | Out-Null
